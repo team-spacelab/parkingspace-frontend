99c5df6522ec7beb478618c96fe151dd2279cf04
Sichu0725 <choihc8511@gmail.com>
2022-09-13 23:45:50 +0900

    mainPage modal

--- src/components/parkingspaceInfoModal.js
+++ src/components/parkingspaceInfoModal.js
@@ -1,17 +1,38 @@
 import React, { useEffect } from 'react'
-import { FaMapMarkedAlt } from 'react-icons/fa'
-import tmpImg from '../assets/addParking.jpg'
+import {
+  FaMapMarkedAlt,
+  FaUserCircle,
+  FaRegClock,
+  FaQrcode,
+} from 'react-icons/fa'
 const ParkingspaceInfoModal = ({ id, setShowModal }) => {
-  useEffect(() => {}, [])
+  useEffect(() => {
+    // id로 썸네일같은거 다 받아오라 이말이야
+  }, [])
   return (
     <>
       <div className='parkingspaceInfoModal'>
         <div className='dragBar' />
         <h3 className='address'>
           <FaMapMarkedAlt />
-          {'하하 fetch문 써라 게이야~'}
+          &nbsp;
+          {'교장쌤 집 앞 주차장'}
         </h3>
-        <img src={tmpImg} alt={tmpImg} />
+        <div className='parkingspaceThum' /> {/** Image임 */}
+        <p>
+          <FaUserCircle /> {'orol4rang'}
+        </p>
+        <p>경상북도 의성군 봉양면 봉호로 75-1</p>
+        <div>
+          <button>
+            <FaQrcode />
+            &nbsp; 현장 구매
+          </button>
+          <button>
+            <FaRegClock />
+            &nbsp; 주차장 예약
+          </button>
+        </div>
       </div>
       <div className='backgroundModal' onClick={() => setShowModal(0)} />
     </>
