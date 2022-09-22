4a8073ef34e147bfdcb11e445dc9e84af27d8435
Minhyeok Park <opensource@pmh.codes>
2022-09-22 22:16:20 +0900

    fix: navigate back

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,10 +1,13 @@
 import '../style/component.scss'
 import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 const Layout = ({ title, onSubmit, buttonText, children, subLink }) => {
+  const navigate = useNavigate()
+
   return (
     <div className='component'>
       <div>
-        <p className='goBackBtn' onClick={() => window.history.back()}>
+        <p className='goBackBtn' onClick={() => navigate('/')}>
           돌아가기
         </p>
         <h2 className='title'>{title}</h2>
