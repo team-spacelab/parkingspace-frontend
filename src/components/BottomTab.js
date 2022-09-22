95099d7c0d6ba45ba6fd2f1911e20861b27f0825
Minhyeok Park <opensource@pmh.codes>
2022-09-22 14:40:29 +0900

    fix: main error

--- src/components/BottomTab.js
+++ src/components/BottomTab.js
@@ -1,11 +1,13 @@
 import { FaParking, FaHome, FaUser } from 'react-icons/fa'
 import { Link } from 'react-router-dom'
 const BottomTab = () => {
+  const location = window.location.pathname
+
   return (
     <div className="bottomTab">
-      <Link to="/parkingspace"><button><FaParking/><p>내 주차장</p></button></Link>
-      <Link to="/"><button><FaHome/><p>홈</p></button></Link>
-      <Link to="/myInfo"><button><FaUser/><p>내 정보</p></button></Link>
+      <Link to="/parkingspace"><button className={location === '/parkingspace' ? 'selected' :''} ><FaParking/><p>내 주차장</p></button></Link>
+      <Link to="/main"><button className={location === '/main' ? 'selected' :''}><FaHome/><p>홈</p></button></Link>
+      <Link to="/myInfo"><button className={location === '/myInfo' ? 'selected' :''}><FaUser/><p>내 정보</p></button></Link>
     </div>
   )
 }
--- src/pages/root.js
+++ src/pages/root.js
@@ -5,14 +5,12 @@ const Root = () => {
   const navigation = useNavigate()
 
   useEffect(() => {
-if (window.localStorage.getItem('main') === '0') {
-      navigation('/main')
-      return
-    }
     if (window.localStorage.getItem('main') === '1') {
       navigation('/parkingspace')
       return
     }
+
+    navigation('/main')
   }, [])
 
   return (
